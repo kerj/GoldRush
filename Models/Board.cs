9410ec8cc4368de1a2fb0d13ba0eeade675851b6
Justin Kerntz and Mathew Akre <justin@>
2019-05-20 11:59:12 -0700

    add updated database

---
+++ Models/Board.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace GoldRush.Models
+{
+  public class Board
+  {
+    public int Id { get; set; }
+    public int Xaxis { get; set; }
+    public int Yaxis { get; set; }
+
+    public Board(int xaxis, int yaxis)
+    {
+      Xaxis = xaxis;
+      Yaxis = yaxis;
+    }
+
+    public BuildBoard()
+    {
+      int xaxis = 10;
+      int yaxis = 10;
+      Board newGame = new Board(xaxis, yaxis);
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      for(int i = 1; i < xaxis+1; i++)
+      {
+        for(int j = 1; j < yaxis+1; i++)
+        {
+          MySqlCommand cmd = new MySqlCommand(@"INSERT INTO boards (x_axis, y_axis) VALUES (@Xaxis, Yaxis)", conn);
+          cmd.Parameters.AddWithValue("@Xaxis", i);
+          cmd.Parameters.AddWithValue("@Yaxis", j);
+        }
+      }
+      Id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+  }
+}
